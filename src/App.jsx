cc01df50f66d4b36012ea407c7ef3a43c105e918
yossefftw <yosseferrazik@gmail.com>
2023-12-18 18:52:37 +0100

    1.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import "./main.css";
+import "./App.css";
 
 export default function App() {
   return (
     <main>
       <header>
-        <h1>Holssssssssssssa</h1>
+        <h1>Me llamo Yossef Errazik</h1>
       </header>
     </main>
   );
--- src/Index.jsx
+++ src/Index.jsx
@@ -2,6 +2,8 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
+import "./Index.css";
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
