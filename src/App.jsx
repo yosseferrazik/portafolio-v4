dbf958eced5bb38d91ebf0c3fcd5e41479bdbb46
yossefftw <yosseferrazik@gmail.com>
2023-12-18 17:41:31 +0100

    1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import "./styles.css";
-import Sidenav from "./components/Sidenav/Sidenav";
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import {
-  FaHeart,
-} from "react-icons/fa";
+import "./main.css";
 
 export default function App() {
   return (
-    <div className="main">
-      <div className="bg"></div>
-      <Sidenav/>  
-      <Home />
-      <About />
-      <Projects />
-      <footer>
-        <p>
-          Hecho con <FaHeart /> por Yossef FTW
-        </p>
-      </footer>
-    </div>
+    <main>
+      <header>
+        <h1>Holssssssssssssa</h1>
+      </header>
+    </main>
   );
 }
