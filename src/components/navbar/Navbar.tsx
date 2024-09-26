af781bfe3aac5bb4f753909d40f1200396755d6c
Yossef FTW <yosseferrazik@gmail.com>
2024-09-26 17:44:24 +0200

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,29 +1,57 @@
 import "./Navbar.css";
-import { RiHome2Line, RiUser5Line, RiFileUserLine } from "react-icons/ri";
+import { RiHome2Line, RiUser5Line, RiFileUserLine,RiProfileLine, RiMedalLine, RiBox3Line, RiLeafLine, RiMailLine  } from "react-icons/ri";
+import { useState } from "react";
 
 const Navbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false); 
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen); 
+  };
+
+
   return (
-    <nav>
-      <div className="right">
-        {" "}
+<nav>
+      <div className="hamburger" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className={`right ${isOpen ? "open" : ""}`}>
         <ul>
           <li>
             <RiHome2Line />
-            &nbsp;<span>Inicio</span>
+            &nbsp;<span>Home</span>
           </li>
           <li>
             <RiUser5Line />
-            &nbsp;<span>Sobre mi</span>
-          </li>{" "}
+            &nbsp;<span>About me</span>
+          </li>
           <li>
             <RiFileUserLine />
-            &nbsp;<span>Habilidades</span>
-          </li>{" "}
-        </ul>
-      </div>
-      <div className="left">
-        <ul>
-          <li>Hola</li>
+            &nbsp;<span>Resume</span>
+          </li>
+          <li>
+            <RiProfileLine />
+            &nbsp;<span>Professional Profiles</span>
+          </li>
+          <li>
+            <RiMedalLine />
+            &nbsp;<span>Certificates</span>
+          </li>
+          <li>
+            <RiBox3Line />
+            &nbsp;<span>Projects</span>
+          </li>
+          <li>
+            <RiLeafLine />
+            &nbsp;<span>7 HHEP</span>
+          </li>
+          <li>
+            <RiMailLine />
+            &nbsp;<span>Contact</span>
+          </li>
         </ul>
       </div>
     </nav>
@@ -31,3 +59,11 @@ const Navbar = () => {
 };
 
 export default Navbar;
+// Otro lado navbar
+/*
+   <div className="left">
+        <ul>
+          <li>Hola</li>
+        </ul>
+      </div>
+*/
